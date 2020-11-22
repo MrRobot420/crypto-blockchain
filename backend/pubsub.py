fb23b607abf755529bc13137aac96f37768f765d
MrRobot420 <max.karlchen@web.de>
2020-11-22 20:33:08 +0100

    add broadcast for transactions

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -5,6 +5,7 @@ from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
 
 from backend.blockchain.block import Block
+from backend.wallet.transaction import Transaction
 
 subscribe_key = "sub-c-a830ad62-2aaa-11eb-9d95-7ab25c099cb1"
 publish_key = "pub-c-c6c3062a-383d-4305-b0af-ed22ec1fe0c9"
@@ -15,13 +16,15 @@ pnconfig.subscribe_key = subscribe_key
 
 CHANNELS = {
     'TEST': 'TEST',
-    'BLOCK': 'BLOCK'
+    'BLOCK': 'BLOCK',
+    'TRANSACTION': 'TRANSACTION'
 }
 
 class Listener(SubscribeCallback):
 
-    def __init__(self, blockchain):
+    def __init__(self, blockchain, transaction_pool):
         self.blockchain = blockchain
+        self.transaction_pool = transaction_pool
 
     def message(self, pubnub, message_object):
         print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
@@ -31,11 +34,16 @@ class Listener(SubscribeCallback):
             potential_chain = self.blockchain.chain[:]
             potential_chain.append(block)
 
-        try:
-            self.blockchain.replace_chain(potential_chain)
-            print(f'\n -- Successfully replaced the local chain.')
-        except Exception as e:
-            print(f'\n -- Did not replace chain: {e}')
+            try:
+                self.blockchain.replace_chain(potential_chain)
+                print(f'\n -- Successfully replaced the local chain.')
+            except Exception as e:
+                print(f'\n -- Did not replace chain: {e}')
+
+        elif message_object.channel == CHANNELS['TRANSACTION']:
+            transaction = Transaction.from_json(message_object.message)
+            self.transaction_pool.set_transaction(transaction)
+            print(f'\n -- Set the new transaction in the transaction pool')
 
 class PubSub():
     """
@@ -43,10 +51,10 @@ class PubSub():
     Provides communication between the nodes of the blockchain network.
     """
 
-    def __init__(self, blockchain):
+    def __init__(self, blockchain, transaction_pool):
         self.pubnub = PubNub(pnconfig)
         self.pubnub.subscribe().channels(CHANNELS.values()).execute()
-        self.pubnub.add_listener(Listener(blockchain))
+        self.pubnub.add_listener(Listener(blockchain, transaction_pool))
 
     
     def publish(self, channel, message):
@@ -61,6 +69,13 @@ class PubSub():
         Broadcast a block object to all nodes.
         """
         self.publish(CHANNELS['BLOCK'], block.to_json())
+
+    
+    def broadcast_transaction(self, transaction):
+        """
+        Broadcast a transaction to all nodes.
+        """
+        self.publish(CHANNELS['TRANSACTION'], transaction.to_json())
 
 
 def main():
