e4722cdc668de1a5dedb5d641f917cbf465b7ba6
MrRobot420 <max.karlchen@web.de>
2020-11-25 22:23:12 +0100

    add check for historic balances

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -1,5 +1,6 @@
 from backend.blockchain.block import Block
 from backend.wallet.transaction import Transaction
+from backend.wallet.wallet import Wallet
 from backend.config import MINING_REWARD_INPUT
 
 class Blockchain:
@@ -70,6 +71,9 @@ class Blockchain:
             last_block = chain[i-1]
             Block.is_valid_block(last_block, block)
 
+        Blockchain.is_valid_transaction_chain(chain)
+
+
     @staticmethod
     def is_valid_transaction_chain(chain):
         """
@@ -80,10 +84,17 @@ class Blockchain:
         """
         transaction_ids = set()
         
-        for block in chain:
+        for i in range(len(chain)):
+            block = chain[i]
             has_mining_reward = False
+
             for transaction_json in block.data:
                 transaction = Transaction.from_json(transaction_json)
+
+                if transaction.id in transaction_ids:
+                    raise Exception(f'Transaction {transaction.id} is not unique')
+
+                transaction_ids.add(transaction.id)
 
                 if transaction.input == MINING_REWARD_INPUT:
                     if has_mining_reward:
@@ -92,11 +103,16 @@ class Blockchain:
                             f'Check block with hash: {block.hash}'
                         )
                     has_mining_reward = True
+                else:
+                    historic_blockchain = Blockchain()
+                    historic_blockchain.chain = chain[0:i]
+                    historic_balance = Wallet.calculate_balance(
+                        historic_blockchain,
+                        transaction.input['address']
+                    )
 
-                if transaction.id in transaction_ids:
-                    raise Exception(f'Transaction {transaction.id} is not unique')
-
-                transaction_ids.add(transaction.id)
+                    if historic_balance != transaction.input['amount']:
+                        raise Exception(f'Transaction {transaction.id} has an invalid input amount')
 
                 Transaction.is_valid_transaction(transaction)
 
