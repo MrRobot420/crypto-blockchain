a96d2d3a4e159bf781da5a648e60cf56b2fbb34a
MrRobot420 <max.karlchen@web.de>
2020-11-26 20:31:24 +0100

    add useState hooks for google search example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
+import React, { useState } from 'react'
+
 function App() {
+  const [userQuery, setUserQuery] = useState('')
+
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery);
+    setUserQuery(event.target.value)
+  }
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`)
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      searchQuery()
+    }
+  } 
+
   return (
     <div className="App">
-     App
+     <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress}/>
+     <button onClick={searchQuery}>Search</button>
     </div>
   );
 }
